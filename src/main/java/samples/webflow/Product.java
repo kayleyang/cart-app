984e502e6b9186669816d2294ce8abc4147e22f2
杨凯 <627134000@qq.com>
2019-06-19 23:17:48 +0800

    CartApp4

---
+++ src/main/java/samples/webflow/Product.java
@@ -0,0 +1,41 @@
+package samples.webflow;
+
+
+public class Product {
+
+    private Integer id;
+
+    private String description;
+
+    private Integer price;
+
+    public Product(Integer id, String description, Integer price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
--- src/main/webapp/WEB-INF/jsp/viewCart.jsp
+++ src/main/webapp/WEB-INF/jsp/viewCart.jsp
@@ -1,3 +1,5 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8" isELIgnored="false" %>
 <?xml version="1.0" encoding="utf-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -9,5 +11,14 @@
 <body>
 <h1>View Cart</h1>
 <a href="${flowExecutionUrl}&_eventId=submit">Submit</a>
+<h2>Products for Your Choice</h2>
+<table>
+    <c:forEach var="product" items="${products}">
+        <tr>
+            <td>${product.description}</td>
+            <td>${product.price}</td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
